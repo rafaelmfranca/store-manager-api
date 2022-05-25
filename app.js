d8840fbc5c8cb2a0ff3aa1daf8a812e4f3240ebb
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-05-25 14:54:10 -0300

    ✨ (middleware): Add generic error func

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const error = require('./middlewares/error');
 
 const app = express();
+
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
+app.use(error);
+
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
---
+++ middlewares/error.js
@@ -0,0 +1,19 @@
+const { StatusCodes } = require('http-status-codes');
+
+function error(err, _req, res, _next) {
+  if (err.isJoi) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: err.details[0].message });
+  }
+
+  if (err.message) {
+    return res.status(err.status).json({ message: err.message });
+  }
+
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ message: 'Internal Server Error' });
+}
+
+module.exports = error;
