966c39c965270854195def9592a1929284f0a60d
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-05-26 14:41:07 -0300

    🤡 (product): Add product mocks

---
+++ test/__mocks__/products.js
@@ -0,0 +1,21 @@
+const products = [
+  {
+    id: 1,
+    name: 'Martelo de Thor',
+    quantity: 10,
+  },
+  {
+    id: 2,
+    name: 'Traje de encolhimento',
+    quantity: 20,
+  },
+  {
+    id: 3,
+    name: 'Escudo do Capitão América',
+    quantity: 30,
+  },
+];
+
+const testProduct = { id: 1, name: 'Test Product', quantity: 10 };
+
+module.exports = { products, testProduct };
