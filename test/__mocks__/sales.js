4f1b7487def396d8e72bbfd295ade3a0e7bde3b0
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-05-26 15:00:12 -0300

    🤡 (sale): Add sale mocks

---
+++ test/__mocks__/sales.js
@@ -0,0 +1,35 @@
+const sales = [
+  {
+    id: 1,
+    date: '2022-05-26T17:35:23.000Z',
+    product_id: 1,
+    quantity: 5,
+  },
+  {
+    id: 1,
+    date: '2022-05-26T17:35:23.000Z',
+    product_id: 2,
+    quantity: 10,
+  },
+  {
+    id: 2,
+    date: '2022-05-26T17:35:23.000Z',
+    product_id: 3,
+    quantity: 15,
+  },
+];
+
+const salesById = [
+  {
+    date: '2022-05-26T17:35:23.000Z',
+    product_id: 1,
+    quantity: 5,
+  },
+  {
+    date: '2022-05-26T17:35:23.000Z',
+    product_id: 2,
+    quantity: 10,
+  },
+];
+
+module.exports = { sales, salesById };
