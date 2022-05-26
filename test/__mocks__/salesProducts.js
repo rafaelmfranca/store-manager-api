ef778de8df2bd502b346a9072fe2cdcb9f18c2e6
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-05-26 15:22:23 -0300

    🤡 (mock): Add sales products mocks

---
+++ test/__mocks__/salesProducts.js
@@ -0,0 +1,8 @@
+const testSale = { saleId: 1, productId: 1, quantity: 1 };
+
+const saleResume = [
+  { product_id: 1, quantity: 5 },
+  { product_id: 2, quantity: 10 },
+];
+
+module.exports = { testSale, saleResume };
